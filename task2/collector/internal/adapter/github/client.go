8cb09f376e1119c423840d97077c638a320e0702
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2026-03-20 00:12:17 +0300

    Implemented logic of task 1 with using GitHub API and main logic into Use Case

---
+++ task2/collector/internal/adapter/github/client.go
@@ -0,0 +1,62 @@
+package adapter
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/SANEKNAYMCHIK/distrib-system/collector/internal/domain"
+)
+
+type GitHubReposClient struct {
+	baseURL string
+	client  *http.Client
+}
+
+func NewGitHubReposClient() *GitHubClient {
+	return &GitHubClient{
+		baseURL: "https://api.github.com/repos",
+		client:  new(http.Client),
+	}
+}
+
+type gitHubRepo struct {
+	Name            string    `json:"name"`
+	Description     string    `json:"description"`
+	StargazersCount int       `json:"stargazers_count"`
+	ForksCount      int       `json:"forks_count"`
+	CreatedAt       time.Time `json:"created_at"`
+}
+
+func (repo gitHubRepo) String() string {
+	res := fmt.Sprintf(
+		"Name: %s\nDescription: %s\nStargazersCount: %d\nForksCount: %d\nCreatedAt: %s\n",
+		repo.Name, repo.Description, repo.StargazersCount, repo.ForksCount, repo.CreatedAt,
+	)
+	return res
+}
+
+func (g *GitHubReposClient) GetRepoInfo(owner, repo string) (domain.Repo, error) {
+	url := fmt.Sprintf("%s/%s/%s", g.baseURL, owner, repo)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("internal problems of server: %s", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("something went wrong: %s", resp.Status)
+	}
+	var repoInfo gitHubRepo
+	if err := json.NewDecoder(resp.Body).Decode(&repoInfo); err != nil {
+		log.Fatalf("incorrect decoding to Go struct: %s", err)
+	}
+	return domain.Repo{
+		Name:        repoInfo.Name,
+		Description: repoInfo.Description,
+		Stargazers:  repoInfo.StargazersCount,
+		Forks:       repoInfo.ForksCount,
+		CreatedAt:   repoInfo.CreatedAt,
+	}, nil
+}
