90ac4eba685834fb4c274d5768d7e84570acc079
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2026-03-20 01:01:59 +0300

    Added main.go for running the service

---
+++ task2/collector/cmd/collector/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	adapter "github.com/SANEKNAYMCHIK/distrib-system/collector/internal/adapter/github"
+	handler "github.com/SANEKNAYMCHIK/distrib-system/collector/internal/handler/grpc"
+	pb "github.com/SANEKNAYMCHIK/distrib-system/collector/internal/proto"
+	"github.com/SANEKNAYMCHIK/distrib-system/collector/internal/usecase"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	githubClient := adapter.NewGitHubReposClient()
+	useCase := usecase.NewRepoUseCase(githubClient)
+	handler := handler.NewRepoHandler(useCase)
+
+	server := grpc.NewServer()
+	pb.RegisterRepoServiceServer(server, handler)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Println("grpc server started")
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
